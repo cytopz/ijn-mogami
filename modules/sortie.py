a9d1fc06ad3ad94badf520595cd7bc5492928bd6
Falskim <fawwazsyihamm@gmail.com>
2020-01-12 03:16:56 +0700

    Add mob kill requirement dictionary

--- modules/sortie.py
+++ modules/sortie.py
@@ -19,8 +19,8 @@ class Sortie:
             'go2': Dimension(864, 554),
             'confirm': Dimension(525, 486)
         }
-        self.sortie_map = '12-1'
-        self.mob_kill_required = 6
+        self.sortie_map = '4-2'
+        self.mob_kill_required = MapDetail(self.sortie_map).kill_requirement
         self.kill_count = 0
         self.switch_boss = True
         self.mob_fleet = 1
@@ -48,6 +48,7 @@ class Sortie:
                 Tools.tap(map_loc)
                 Tools.tap(self.buttons['go1'])
             Tools.tap(self.buttons['go2'])
+            print(self.mob_kill_required, "mob required to kill")
         else:
             print("map not found")
         Tools.wait(7)
@@ -336,3 +337,74 @@ class Sortie:
         Tools.tap(Dimension(785, 621))
         Tools.tap(self.buttons['back'])
         Tools.wait(7)
+
+class MapDetail:
+    def __init__(self, sortie_map):
+        self.battle_requirement = {
+            # Chapter 1
+            '1-1': 1,
+            '1-2': 2,
+            '1-3': 2,
+            '1-4': 3,
+            # Chapter 2
+            '2-1': 2,
+            '2-2': 3,
+            '2-3': 3,
+            '2-4': 3,
+            # Chapter 3
+            '3-1': 3,
+            '3-2': 3,
+            '3-3': 3,
+            '3-4': 3,
+            # Chapter 4
+            '4-1': 3,
+            '4-2': 3,
+            '4-3': 3,
+            '4-4': 4,
+            # Chapter 5
+            '5-1': 4,
+            '5-2': 4,
+            '5-3': 4,
+            '5-4': 4,
+            # Chapter 6
+            '6-1': 4,
+            '6-2': 4,
+            '6-3': 4,
+            '6-4': 5,
+            # Chapter 7
+            '7-1': 5,
+            '7-2': 5,
+            '7-3': 5,
+            '7-4': 5,
+            # Chapter 8
+            '8-1': 4,
+            '8-2': 4,
+            '8-3': 4,
+            '8-4': 5,
+            # Chapter 9
+            '9-1': 5,
+            '9-2': 5,
+            '9-3': 5,
+            '9-4': 5,
+            # Chapter 10
+            '10-1': 6,
+            '10-2': 6,
+            '10-3': 6,
+            '10-4': 6,
+            # Chapter 11
+            '11-1': 6,
+            '11-2': 6,
+            '11-3': 6,
+            '11-4': 6,
+            # Chapter 12
+            '12-1': 6,
+            '12-2': 6,
+            '12-3': 6,
+            '12-4': 6,
+            # Chapter 13
+            '13-1': 6,
+            '13-2': 6,
+            '13-3': 6,
+            '13-4': 7
+        }
+        self.kill_requirement = self.battle_requirement[sortie_map]
