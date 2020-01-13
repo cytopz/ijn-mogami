5f8db4134733bf1fee855c6899123e9ec93f7a87
Falskim <fawwazsyihamm@gmail.com>
2020-01-14 02:35:25 +0700

    Fix and add stuff
    - Retirement filtering
    - Print formatting (?)
    - Sortie time elapse
    - Fix tapping at battle formation
    - Fix end battle handler (placeholder?)
    - Etc (i forgot lol)

--- main.py
+++ main.py
@@ -6,16 +6,25 @@
 #   Android Version : 6.0            #
 ######################################
 
+import sys
+from datetime import datetime
 from modules.sortie import Sortie
 
 class Main:
     def __init__(self):
-        self.sortie_module = Sortie()
+        self.sortie_module = Sortie()   
     def start(self):
+        print('Starting sortie...')
         self.sortie_module.start()
+    def print_time_elapse(self):
+        print('Sortie finished at {}'.format(self.sortie_module.get_time_elapse()))
 
 if __name__ == '__main__':
-    while True:
-        mogami = Main()
-        mogami.start()
-        print("done")
+    try:
+        while True:
+            mogami = Main()
+            mogami.start()
+            mogami.print_time_elapse()
+    except KeyboardInterrupt:
+            print("Exiting...")
+            sys.exit(0)
