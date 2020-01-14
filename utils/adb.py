e0fb4d9ecbc473967b761d0b6ea41bd1b7a09794
cytopz <cytopz@waifu.club>
2020-01-14 19:18:46 +0700

    use sys.exit instead of exit

--- utils/adb.py
+++ utils/adb.py
@@ -1,11 +1,12 @@
 import subprocess
+import sys
 
 class Adb:
     def __init__(self):
         self.start_server()
         if not self.device_available:
             print("No devices detected.")
-            exit()
+            sys.exit(1)
 
     def start_server(self):
         try:
@@ -13,7 +14,7 @@ class Adb:
         except Exception as e:
             print('adb was not found. exiting...')
             print(e)
-            exit()
+            sys.exit(1)
 
     def device_available(self):
         devices = subprocess.Popen('adb devices'.split(' '),
